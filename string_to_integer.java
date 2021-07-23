8cbe5ad1ee0f2312bd74ca23281dc5526e61291d
sunny62898 <85967575+sunny62898@users.noreply.github.com>
2021-07-24 00:03:46 +0800

    finished string to integer

--- string_to_integer.java
+++ string_to_integer.java
@@ -1,115 +1,7 @@
 class Solution_StringToInteger {
     public int myAtoi(String s) {
-        /*if(s.length() == 0){
-            return 0;
-        }
-        else if(s.length() >= 2 && (s.charAt(0) == '+' || s.charAt(0) == '-')){
-            if(s.charAt(0) == '-'){
-                if(s.charAt(1) == '+' || s.charAt(1) == '-'){
-                    return 0;
-                }
-            }
-            else if(s.charAt(0) == '+'){
-                if(s.charAt(1) == '+' || s.charAt(1) == '-'){
-                    return 0;
-                }
-            }
-        }
 
-        if((s.charAt(0) >= 'A' && s.charAt(0) <= 'Z') || (s.charAt(0) >= 'a' && s.charAt(0) <= 'z')){
-            return 0;
-
-        }
-        else{
-            boolean no = false;
-            boolean plus = false;
-            boolean neg = false;
-            boolean charater = false;
-            boolean zero = false;
-            boolean Hnum = false;
-            long num = 0;
-            for(int i = 0;i < s.length();i++){
-                char c = s.charAt(i);
-                
-                if(no && !(c >= '0' && c <= '9')){
-                    return 0;
-                }
-
-                else if(Hnum && c == '-'){
-                    break;
-                }
-                else if(plus && (c == ' ' || c == '+')){
-                    return 0;
-                }
-                else if(zero && c == '-'){
-                    return 0;
-                }
-
-                else if(i == 0 && c == '-'){
-                    no = true;
-                    neg = true;
-                }
-                else if(i != 0 && c == '-'){
-                    no = true;
-                    neg = true;
-                }
-                
-                else if(c >= '0' && c <= '9'){
-                    Hnum = true;
-                    if(charater){
-                        break;
-                    }
-                    if(c == '0'){
-                        zero = true;
-                    }
-                    num = num*10 + (c - '0');
-                    
-                    if(num > Integer.MAX_VALUE){
-                        
-                        if(neg){
-                            return Integer.MIN_VALUE;
-                        }
-                        else{
-                            
-                            return Integer.MAX_VALUE;
-                        }
-                        
-                    }
-                    
-                }
-                else if((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')){
-                    charater = true;
-                }
-                else{
-                    if(Hnum){
-                        break;
-                    }
-                    else if(c == ' ' || c == '+'){
-                        no = true;
-                        if(c == '+'){
-                            plus = true;
-                        }
-                        continue;
-                    }
-                    else{
-                        break;
-                    }
-                    
-                }
-            }
-            if(neg){
-                return (-1)*(int)num;
-            }
-            else{
-                
-                return (int)num;
-            }
-            
-        }*/
-
-
-
-        final int len = s.length();
+        /*final int len = s.length();
         if(len < 1) return 0;
         
         int i = 0, ret = 0;
@@ -143,7 +35,82 @@ class Solution_StringToInteger {
         }
               
         return ret;
-    
+    */
+
+        int i = 0;
+        while(i < s.length() && s.charAt(i) == ' '){  //讀掉前面的空白
+            i++;
+        }
+
+        long sum = 0;
+        boolean neg = false;
+        boolean pos = false;
+        boolean num = false;
+        for(int j = i;j < s.length();j++){
+            if(s.charAt(j) >= '0' && s.charAt(j) <= '9'){
+                num = true;
+                sum = sum*10 + (s.charAt(j) - '0');
+                if(sum > Integer.MAX_VALUE){
+                    if(neg){
+                        return Integer.MIN_VALUE;
+                    }
+                    else{
+                        return Integer.MAX_VALUE;
+                    }
+                }
+            }
+            else if(neg || pos){
+                if(sum > Integer.MAX_VALUE){
+                    if(neg){
+                        return Integer.MIN_VALUE;
+                    }
+                    else{
+                        return Integer.MAX_VALUE;
+                    }
+                }
+                else{
+                    if(neg){
+                        return (-1)*(int)(sum);
+                    }
+                    else{
+                        return (int)(sum);
+                    }
+                }
+            }
+            else if(!num && s.charAt(j) == '-'){
+                neg = true;
+            }
+            else if(!pos && s.charAt(j) == '+'){
+                pos = true;
+            }
+            
+            else{
+                if(sum > Integer.MAX_VALUE){
+                    if(neg){
+                        return Integer.MIN_VALUE;
+                    }
+                    else{
+                        return Integer.MAX_VALUE;
+                    }
+                }
+                else{
+                    if(neg){
+                        return (-1)*(int)(sum);
+                    }
+                    else{
+                        return (int)(sum);
+                    }
+                }
+                
+            }
+        }
+       
+        if(neg){
+            return (-1)*(int)(sum);
+        }
+        else{
+            return (int)(sum);
+        }
     }
 }
 
@@ -153,7 +120,7 @@ public class string_to_integer {
     public static void main(String[] args) {
         Solution_StringToInteger test = new Solution_StringToInteger();
         
-        String str = "++1";
+        String str = "9223372036854775808";
         int ans = test.myAtoi(str);
         System.out.println(ans);
     }
